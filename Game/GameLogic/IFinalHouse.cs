77150be8d488f934820e2cec216c8cbf46feb9c0
OxyEho <valera808@inbox.ru>
2020-05-08 20:28:37 +0500

    aaaaa

---
+++ Game/GameLogic/IFinalHouse.cs
@@ -0,0 +1,7 @@
+﻿namespace Game.GameLogic
+{
+    public interface IFinalHouse
+    {
+        
+    }
+}
--- Game/Views/GameForm.cs
+++ Game/Views/GameForm.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System.Drawing;
+using System.Windows.Forms;
 
 namespace Game.Views
 {
@@ -8,9 +9,29 @@ namespace Game.Views
         public GameForm(GameLogic.Game game)
         {
             this.game = game;
+            MinimumSize = new Size(1200, 800);
+            MaximumSize = MinimumSize;
+            var table = new TableLayoutPanel();
+            for (var i = 0; i < 10; i++)
+            {
+                table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10));
+                if (i == 0 || i == 1 || i == 2)
+                    table.RowStyles.Add(new RowStyle(SizeType.Percent, 34));
+            }
+
+            for (var i = 0; i < 10; i++)
+            {
+                for (var j = 0; j < 3; j++)
+                {
+                    var button = new Button {Dock = DockStyle.Fill};
+                    table.Controls.Add(button, i, j);
+                }
+            }
+            
+            table.BorderStyle = BorderStyle.Fixed3D;
+            table.Width = 1000;
+            table.Height = 400;
+            Controls.Add(table);
         }
     }
-    
-    
-    
 }
